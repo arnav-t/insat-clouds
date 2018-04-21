ca1380e0a98a0d7386425b089b97a79a5fd70a16
Arnav Tiwari <34343421+arnav-t@users.noreply.github.com>
2018-04-21 15:59:11 +0000

    Add files via upload

--- cloudmap.py
+++ cloudmap.py
@@ -6,15 +6,20 @@ mapURL = 'http://satellite.imd.gov.in/img/3Dasiasec_bt1.jpg'
 currDir = '/home/avz/Documents/py/insat/'
 
 def saveMap(count):
-	urllib.request.urlretrieve(mapURL, currDir+str(count) + '.jpg')
-
-t = time.localtime()
-curtime = time.strftime('%b-%d-%Y_%R:%S', t)
+	urllib.request.urlretrieve(mapURL, currDir + str(count) + '.jpg')
 
 currentMap = 0
 
 if os.stat(currDir + 'maps.log').st_size:
-	with open(currDir + 'maps.log','r') as logfile:
-		
+	with open(currDir + 'maps.log', 'r') as logfile:
+		for line in logfile:
+			pass
+		currentMap = (int(line[-6]) + 1)%10
 
-saveMap(1)
+t = time.localtime()
+currTime = time.strftime('%b-%d-%Y_%R:%S', t)
+
+saveMap(currentMap)
+with open(currDir + 'maps.log', 'a') as logfile:
+	logfile.write(currTime + ' ' + str(currentMap) + '.jpg\n')
+
